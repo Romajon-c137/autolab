02180b103b4225c0d1170379e241e808aa0781f7
Autolab Server <muhammadzohidmuhtarjanov@gmail.com>
2026-09-07 05:04:52 +0000

    Merge O3-O4 and milestone migrations

---
+++ backend/inspections/migrations/0038_merge_milestone_and_o3_o4.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("inspections", "0036_userprofile_milestone_1000_acknowledged_at"),
+        ("inspections", "0037_seed_o3_o4_price"),
+    ]
+
+    operations = []
